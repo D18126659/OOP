b815a68116d90b4da42b96211a7962f4414472f8
D18126659 <55544206+D18126659@users.noreply.github.com>
2019-10-17 15:33:07 +0100

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,38 @@
+import math
+print("Please type  1 number:")
+num1 = input()
+val1 = int(num1)
+print("Please enter another number")
+num2 = input()
+val2 = int(num2)
+
+
+def gcd(a, b):
+    return math.gcd(a, b)
+
+
+print("gcd: ", math.gcd(val1, val2))
+
+
+def lcm(a, b):
+    return lcm(a, b)
+
+
+print("lcm: ", lcm(val1, val2))
+
+
+def addfrac(a, b):
+    return addfrac(a, b)
+
+
+print("addFrac:", addfrac(val1, val2))
+
+
+def subfrac(a, b):
+    return subfrac(a, b)
+
+
+print("subFrac:", subfrac(val1, val2))
+
+
+
